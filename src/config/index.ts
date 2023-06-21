09141b4c9ce3299322b9d1c89ca999f1fb6f64a8
devabormon <dev.abormon28@gmail.com>
2023-06-21 19:41:38 +0600

    add example env

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,5 +6,5 @@ dotenv.config({ path: path.join(process.cwd(), '.env') });
 export default {
     env: process.env.NODE_ENV,
     port: process.env.PORT,
-    database_url: process.env.DABASE_URL,
+    database_url: process.env.DATABASE_URL,
 };
